14ffb2b15b78765fb8311e16999d612002b260c4
frsong <song.francis@gmail.com>
2016-07-01 17:25:48 -0400

    Initial commit

--- examples/models/multisensory.py
+++ examples/models/multisensory.py
@@ -32,7 +32,11 @@ n_gradient   = n_conditions
 n_validation = 100*n_conditions
 
 # Input noise
-sigma = np.sqrt(2*100*0.019)
+sigma = np.sqrt(2*100*0.02)
+
+lr          = 0.002
+baseline_lr = 0.002
+tau_reward  = 15000
 
 # Separate visual and auditory inputs
 N    = 100
