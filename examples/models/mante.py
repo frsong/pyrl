d98d96942d2f9990dd4ea1ea180a91eaac618474
frsong <song.francis@gmail.com>
2016-07-03 07:53:30 -0400

    Initial commit

--- examples/models/mante.py
+++ examples/models/mante.py
@@ -32,7 +32,7 @@ n_gradient   = n_conditions
 n_validation = 100*n_conditions
 
 # Input noise
-sigma = np.sqrt(2*100*0.025)
+sigma = np.sqrt(2*100*0.023)
 
 # Rewards
 R_ABORTED = -1
--- examples/models/multisensory.py
+++ examples/models/multisensory.py
@@ -33,7 +33,7 @@ n_gradient   = n_conditions
 n_validation = 100*n_conditions
 
 # Input noise
-sigma = np.sqrt(2*100*0.025)
+sigma = np.sqrt(2*100*0.023)
 
 # Separate visual and auditory inputs
 '''
