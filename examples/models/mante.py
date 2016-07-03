f4e2c43b66c92ac4b5b8fa06109916c48624ab82
frsong <song.francis@gmail.com>
2016-07-03 12:38:41 -0400

    Initial commit

--- examples/models/mante.py
+++ examples/models/mante.py
@@ -28,11 +28,11 @@ cohs         = [5, 15, 50]
 n_conditions = len(contexts) * (len(left_rights)*len(cohs))**2
 
 # Training
-n_gradient   = n_conditions
-n_validation = 100*n_conditions
+n_gradient   = 2*n_conditions
+n_validation = 50*n_conditions
 
 # Input noise
-sigma = np.sqrt(2*100*0.023)
+sigma = np.sqrt(2*100*0.02)
 
 # Rewards
 R_ABORTED = -1
