92fa6a98b9d1bd8957b8cb7bfd34784f70fdc2ba
frsong <song.francis@gmail.com>
2016-07-03 15:42:42 -0400

    Initial commit

--- pyrl/nptools.py
+++ pyrl/nptools.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def get_rng(seed=1, loc='Not specified'):
+def get_rng(seed=0, loc='Not specified'):
     print("[ RNG {} ] {}".format(seed, loc))
 
     return np.random.RandomState(seed)
