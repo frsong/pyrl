98d47f34e889a043f3d9e71c1a9e26bcc6c0200f
frsong <song.francis@gmail.com>
2016-07-04 14:05:40 -0400

    Initial commit

--- examples/models/multisensory.py
+++ examples/models/multisensory.py
@@ -39,11 +39,11 @@ N    = 100
 Wins = []
 for i in xrange(3):
     Win = np.zeros((len(inputs), N))
-    Win[inputs['FIXATION']]               = 1
-    Win[inputs['VISUAL-P'],:N//3]         = 1
-    Win[inputs['VISUAL-N'],:N//3]         = 1
-    Win[inputs['AUDITORY-P'],N//3:2*N//3] = 1
-    Win[inputs['AUDITORY-N'],N//3:2*N//3] = 1
+    Win[inputs['FIXATION']]          = 1
+    Win[inputs['VISUAL-P'],:N//3]    = 1
+    Win[inputs['VISUAL-N'],:N//3]    = 1
+    Win[inputs['AUDITORY-P'],-N//3:] = 1
+    Win[inputs['AUDITORY-N'],-N//3:] = 1
     Wins.append(Win)
 Win      = np.concatenate(Wins, axis=1)
 Win_mask = Win.copy()
