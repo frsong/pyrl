c2a7d0c2d51f38f2f4b9a0b5d13dab651a0025d1
frsong <song.francis@gmail.com>
2016-07-06 16:26:07 -0400

    Initial commit

--- examples/models/mante.py
+++ examples/models/mante.py
@@ -28,7 +28,7 @@ cohs         = [5, 15, 50]
 n_conditions = len(contexts) * (len(left_rights)*len(cohs))**2
 
 # Sample logarithmically to get symmetric psychometric curves
-log_min_coh = np.log(min(cohs)/1.5)
+log_min_coh = np.log(min(cohs)/2)
 log_max_coh = np.log(max(cohs)*1.5)
 
 # Training
@@ -36,7 +36,7 @@ n_gradient   = n_conditions
 n_validation = 50*n_conditions
 
 # Input noise
-sigma = np.sqrt(2*100*0.022)
+sigma = np.sqrt(2*100*0.02)
 
 # Rewards
 R_ABORTED = -1
