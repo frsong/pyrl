69d740c83d9c8691c3ea8e6bd9822e30f5e0eb88
frsong <song.francis@gmail.com>
2016-07-30 19:33:51 -0400

    Initial commit

--- paper/all.py
+++ paper/all.py
@@ -224,7 +224,7 @@ ntrials_a = 100
 
 if 'multisensory' in args:
     print("=> Multisensory integration")
-    seed = 97
+    seed = 96
     train(model, seed=seed, main=True)
     trials(model, 'b', ntrials_b)
     do_action(model, 'psychometric')
